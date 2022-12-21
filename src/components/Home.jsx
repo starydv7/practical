30477b33b0893a69c51a0464f2eebd28bfefc90d
starydv7 <pawanstar72926@gmail.com>
2022-12-21 21:22:58 +0530

    practical

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,24 @@ import styles from "./home.module.css";
 import { useState } from 'react';
 const Home = () => {
     const [data, setData] = useState([]);
-    const getData = () => {
-        axios.get("")
-    }
+    const url = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
+    fetch(url, {
+      method: "GET",
+      withCredentials: true,
+      headers: {
+        "X-Auth-Token": "6200f750b9d3f148c5712e63",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then(function (data) {
+          console.log(data);
+          setData(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+   
   return (
       <div>
           
