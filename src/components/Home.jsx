08be4737abf85c9b37f9f5f219951917e7e5b6d1
starydv7 <pawanstar72926@gmail.com>
2022-12-21 21:29:44 +0530

    fetch data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,25 +2,32 @@ import React from 'react'
 import axios from "axios";
 import styles from "./home.module.css";
 import { useState } from 'react';
+import { useEffect } from 'react';
 const Home = () => {
     const [data, setData] = useState([]);
-    const url = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
-    fetch(url, {
-      method: "GET",
-      withCredentials: true,
-      headers: {
-        "X-Auth-Token": "6200f750b9d3f148c5712e63",
-        "Content-Type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then(function (data) {
-          console.log(data);
-          setData(data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    const url = "https://dummyapi.io/data/v1/user";
+    const getData = () => {
+ fetch(url, {
+   method: "GET",
+   withCredentials: true,
+   headers: {
+     "app-id": "6200f750b9d3f148c5712e63",
+     "Content-Type": "application/json",
+   },
+ })
+   .then((resp) => resp.json())
+   .then(function (data) {
+     console.log(data);
+     setData(data);
+   })
+   .catch(function (error) {
+     console.log(error);
+   });
+    }
+    useEffect(() => {
+        getData();
+    }, []);
+   
    
   return (
       <div>
